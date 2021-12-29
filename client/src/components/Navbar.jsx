ed0b2741e4d9a843b98da4633cca5cc1b4bf144d
Obaid Rustemi <orustemi@gmail.com>
2021-12-29 00:35:15 -0500

    add components and more tailwind styling

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { HiMenuAlt4 } from 'react-icons/hi'
+import { AiOutlineClose } from 'react-icons/ai'
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    )
+}
+
+export default Navbar
